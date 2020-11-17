e0ce1f2390a22236beeb5fc77b07cb1191e64322
natalia.castro <nati-ct4@hotmail.com>
2020-11-17 13:02:22 -0500

    Agregamos el index y la carpeta vistas del proyecto inicial

---
+++ vistas/template.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FORMULARIO</title>
+</head>
+<body>
+
+	<form>
+		<p>Usuario:<input type="text" name="user"></p>
+		<p>Contraseña:<input type="text" name="pass"></p>
+		<p>eMail:<input type="text" name="email"></p>
+
+	</form>
+
+</body>
+</html>
